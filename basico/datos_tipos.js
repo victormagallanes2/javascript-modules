0ad1747ca06891760ec426749ee999ccfe60ea1c
victor <victor@debian>
2018-02-13 23:22:24 -0400

    variables y constantes

---
+++ basico/datos_tipos.js
@@ -0,0 +1,49 @@
+var string = "loquesea"
+var stringNumero = "100"
+var integer = 23
+var flotante = 20.56
+var bulean = true
+
+var a = 27
+var b = 4
+var c = 27 / 4
+
+
+// Convertir un numero string a numero se usa
+// la funcion Number
+
+var covercion = Number(stringNumero)
+
+// Tambien existe las funciones parseInt() y parseFloat()
+// que sirver para convertir datos a int o float
+
+var par_int = parseInt(flotante)
+var par_float = parseFloat(flotante)
+
+// Tambien se puede convertir un numero a string
+// usando la funcion String()
+
+var conver_num = String(integer)
+
+// Se puede deternar si un valor es verdadero o falso
+// usando Boolean()
+
+var probar = Boolean(10 > 5)
+
+
+// Para saber que tipo de datos estamos usando
+// se usa la funcion of
+
+
+console.log(string)
+console.log(stringNumero)
+console.log(integer)
+console.log(flotante)
+console.log(bulean)
+console.log(c)
+console.log(stringNumero)
+console.log(par_int)
+console.log(par_float)
+console.log(conver_num)
+console.log(probar)
+console.log(typeof probar)
---
+++ basico/var_const.js
@@ -0,0 +1,8 @@
+// Las variables se declaran con La
+// palabra reservada var
+var mi_variable = 35
+
+// Las constantes se declaran con La
+// palabra reservada var
+
+const mi_variable = 35
